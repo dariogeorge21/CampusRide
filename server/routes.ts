aa48e8052445dd7027c3008c0bd24f96bcb2d2d7
joseph babu <josehp304@gmail.com>
2025-08-09 13:57:31 +0530

    added dotenv dependency

--- server/routes.ts
+++ server/routes.ts
@@ -1,6 +1,7 @@
 import type { Express } from "express";
 import { createServer, type Server } from "http";
 import { storage } from "./storage";
+import 'dotenv/config';
 import { 
   insertStudentSchema, 
   insertBusRouteSchema, 
